e0ccdd1cd8061c8e32c4cba5016f203a7a394faf
Piotr Orzechowski <orzechow@posteo.de>
2016-03-20 23:43:53 +0100

    fixed todo lists

--- doxy-boot.js
+++ doxy-boot.js
@@ -90,7 +90,20 @@ $( document ).ready(function() {
 		if(getOriginalWidthOfImg($(this)[0]) > $('#content>div.container').width())
 			$(this).css('width', '100%');
 	});
-	
+
+
+  /* todo list */
+  var todoelements = $('.contents > .textblock > dl.reflist > dt, .contents > .textblock > dl.reflist > dd');
+  for (var i = 0; i < todoelements.length; i += 2) {
+    $('.contents > .textblock').append(
+      '<div class="panel panel-default active">'
+        + "<div class=\"panel-heading todoname\">" + $(todoelements[i]).html() + "</div>"
+        + "<div class=\"panel-body\">" + $(todoelements[i+1]).html() + "</div>"
+      + '</div>');
+  }
+  $('.contents > .textblock > dl').remove();
+
+
 	$(".memitem").removeClass('memitem');
     $(".memproto").removeClass('memproto');
     $(".memdoc").removeClass('memdoc');
